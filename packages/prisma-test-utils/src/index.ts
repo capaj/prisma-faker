e4b140f84e5ca8532ea05e6e6420c151a048e914
Divyendu Singh <singh@prisma.io>
2019-06-15 15:44:41 +0200

    restructure for simple publishing

--- packages/prisma-test-utils/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { seed } from '../../prisma-faker/dist'
-export * from '../../prisma-faker/dist'
-
-export { Pool } from '../../prisma-db-pool/dist'
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { seed, SeedOptions } from './prisma-faker'
+export * from './prisma-faker'
+
+export { Pool, DBInstance, PoolDefinition } from './prisma-db-pool'
